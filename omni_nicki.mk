5f9d444525eec48184ea2c67f14c875e10793359
Aniruddh Chandratre <chandratre.aniruddh@gmail.com>
2014-08-18 15:56:28 +0530

    I have no idea what I am doing :v

--- omni_nicki.mk
+++ omni_nicki.mk
@@ -16,9 +16,6 @@
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
 # Inherit from our omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
